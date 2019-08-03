7cda6c8e62546bf927fe26dd145edc68f307f8a0
Jordan Grace <jordan.t.grace@gmail.com>
2019-08-03 17:54:09 -0400

    File chooser now choose first file it finds

--- src/mapper.rs
+++ src/mapper.rs
@@ -3,31 +3,28 @@ use std::io;
 use std::path::{Path, PathBuf};
 
 use hyper::{Body, Request, Response, StatusCode, Method};
-use hyper::header::{CONTENT_TYPE, HeaderValue};
+use hyper::header::{self, HeaderValue};
 
 pub fn get_body(base_path: &str, req: Request<Body>) -> io::Result<Response<Body>> {
     let mut response = Response::new(Body::empty());
+
+    let path = PathBuf::from(format!("{}{}", base_path, req.uri().path()));
+    let file_path = choose_file(&path, req.method())?;
+
+    dbg!(&file_path);
     
     if let Some(path) = req.uri().path().split('?').nth(0) {
-        let path = PathBuf::from(format!("{}{}", base_path, path));
-        
-        if path.is_dir() {
-            for entry in fs::read_dir(path)? {
-                let entry_path = entry?.path();
-                if entry_path.is_file() {
-                    let body_text = fs::read_to_string(&entry_path)?;
-                    *response.body_mut() = Body::from(body_text);
+        let body_text = fs::read_to_string(&file_path)?;
+        *response.body_mut() = Body::from(body_text);
 
-                    if let Some(ext) = entry_path.extension() {
-                        if ext == "json" {
-                            response.headers_mut().insert(CONTENT_TYPE, HeaderValue::from_static("application/json"));
-                        }
-                    }
-
-                    return Ok(response);
-                }
+        if let Some(ext) = file_path.extension() {
+            if ext == "json" {
+                response.headers_mut().insert(header::CONTENT_TYPE, HeaderValue::from_static("application/json"));
             }
         }
+
+        return Ok(response);
+
     }
 
    *response.status_mut() = StatusCode::NOT_FOUND;
@@ -35,6 +32,15 @@ pub fn get_body(base_path: &str, req: Request<Body>) -> io::Result<Response<Body
     Ok(response)
 }
 
-pub fn choose_file(path: &Path, method: &Method) -> Option<PathBuf> {
-    None
+pub fn choose_file(path: &Path, method: &Method) ->io::Result<PathBuf> {
+    let available_files = fs::read_dir(path)?
+        .filter_map(Result::ok)
+        .map(|file| file.path())
+        .filter(|path| path.is_file())
+        .collect::<Vec<PathBuf>>();
+
+    match available_files.into_iter().nth(0) {
+        Some(file) => Ok(file),
+        None => Err(io::Error::new(io::ErrorKind::NotFound, "No files available")),
+    }
 }
