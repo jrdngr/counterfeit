497a606f26c83690f777e2b005da41a04e078707
Jordan Grace <jordan.t.grace@gmail.com>
2019-08-14 20:52:35 -0400

    Implemented immutable mutability handler

--- src/mapper/mutation_handler.rs
+++ src/mapper/mutation_handler.rs
@@ -1,8 +1,15 @@
 use std::io;
-use std::path::{Path, PathBuf};
 
-use hyper::{Body, Request, Response, StatusCode};
+use hyper::{Body, Request};
 
 pub trait MutationHandler {
     fn apply_mutation(&mut self, request: &Request<Body>) -> io::Result<()>;
+}
+
+pub struct ImmutableHandler;
+
+impl MutationHandler for ImmutableHandler {
+    fn apply_mutation(&mut self, _request: &Request<Body>) -> io::Result<()> {
+        Ok(())
+    }
 }
